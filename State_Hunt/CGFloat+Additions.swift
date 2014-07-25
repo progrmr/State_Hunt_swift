ffdc21baba4e1f255f6da6c02e4f7fd8ab487bcd
Gary Morris <garym@garym-mac-mini.home>
2014-07-25 06:40:22 -0700

    added CGFloat+Additions to fix problems with Xcode Beta4 type conversions to CGFloat

---
+++ State_Hunt/CGFloat+Additions.swift
@@ -0,0 +1,44 @@
+//
+//  CGFloat+Additions.swift
+//  State_Hunt
+//
+//  Created by Gary Morris on 7/24/14.
+//  Copyright (c) 2014 Gary Morris. All rights reserved.
+//
+
+extension CGFloat {
+    init(_ value: UInt32) {
+        self.init( UInt(value) )
+    }
+}
+
+extension CGFloat {
+    init(_ value: UInt16) {
+        self.init( UInt(value) )
+    }
+}
+
+extension CGFloat {
+    init(_ value: UInt8) {
+        self.init( UInt(value) )
+    }
+}
+
+extension CGFloat {
+    init(_ value: Int32) {
+        self.init( Int(value) )
+    }
+}
+
+extension CGFloat {
+    init(_ value: Int16) {
+        self.init( Int(value) )
+    }
+}
+
+extension CGFloat {
+    init(_ value: Int8) {
+        self.init( Int(value) )
+    }
+}
+
